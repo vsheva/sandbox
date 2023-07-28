b43b721345b4a410fff2c659b1266b086ebaa6ee
vsheva <valera.sheva@gmail.com>
2023-07-28 12:00:03 -0400

    Solution

--- 07-events/02-mouse-events/shopping-list/script.js
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -1,1 +1,20 @@
 
+const logo = document.querySelector("img");
+
+const onClick = () =>{
+    console.log("click event")
+}
+
+const onDoubleClick = () =>{
+    if(document.body.style.backgroundColor !=="purple") {
+        document.body.style.backgroundColor ="purple";
+        document.body.style.color = "black"
+    } else {
+        document.body.style.backgroundColor ="green";
+        document.body.style.color = "white"
+    }
+
+}
+
+logo.addEventListener("click", onClick)
+logo.addEventListener("dblclick", onDoubleClick)
