55b0239931239e6f1f63fdb8a48f96f12575b11b
vsheva <valera.sheva@gmail.com>
2023-07-28 15:26:59 -0400

    Solution

---
+++ 07-events/05-event-keycode-project/event-keycodes/script.js
@@ -0,0 +1,23 @@
+
+window.addEventListener("keydown", function(e) {
+    const insert = document.querySelector("#insert")
+    console.log(insert)
+
+    insert.innerHTML =`
+    <div class="key">
+        ${e.key ===" "? "Space": e.key }
+        <small>e.key</small>
+      </div>
+
+      <div class="key">
+        ${e.keyCode}
+        <small>e.keyCode</small>
+      </div>
+
+      <div class="key">
+        ${e.code}
+        <small>event.code</small>
+      </div>
+    
+    `
+})
