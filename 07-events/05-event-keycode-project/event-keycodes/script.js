537413a7de8833754741a1bdcc5499ce246d9390
vsheva <valera.sheva@gmail.com>
2023-08-09 22:39:09 -0400

    Solution

--- 07-events/05-event-keycode-project/event-keycodes/script.js
+++ 07-events/05-event-keycode-project/event-keycodes/script.js
@@ -1,81 +1,61 @@
-
-
-
-window.addEventListener("keydown", (e)=>{
-
-    const insert= document.querySelector("#insert");
-
-    insert.innerHTML =`
-    <div class="key">
-        ${e.key===" "? "Spaсe" :e.key}
-        <small>e.key</small>
-      </div>
-
-      <div class="key">
-        ${e.keyCode}
-        <small>e.keyCode</small>
-      </div>
-
-      <div class="key">
-        ${e.code}
-        <small>event.code</small>
-      </div>
-    
-    `
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// window.addEventListener("keydown", (e)=>{
+//
+//     const insert= document.querySelector("#insert");
+//
+//     insert.innerHTML =`
+//     <div class="key">
+//         ${e.key===" "? "Spaсe" :e.key}
+//         <small>e.key</small>
+//       </div>
+//
+//       <div class="key">
+//         ${e.keyCode}
+//         <small>e.keyCode</small>
+//       </div>
+//
+//       <div class="key">
+//         ${e.code}
+//         <small>event.code</small>
+//       </div>
+//
+//     `
+//
+// })
 
 
 //2
 
-//
-// const keyDownHandler=(e)=>{
-//     const insert = document.querySelector("#insert")
-//
-//     const keyObj = {
-//         "e.key ": e.key ===" "? "Space": e.key,
-//         "e.keyCode": e.keyCode,
-//         "e.code": e.code
-//     }
-//
-//     for(let key in keyObj) {
-//         const div= document.createElement("div");
-//         div.innerHTML = ""
-//         div.className = "key"
-//
-//         console.log(div)
-//
-//         const small= document.createElement("small");
-//         const smallTextKey=document.createTextNode(key)
-//
-//         const divKeyValueText= document.createTextNode(keyObj[key]);
-//
-//         small.appendChild(smallTextKey);
-//         div.appendChild(divKeyValueText);
-//         div.appendChild(small)
-//         insert.appendChild(small)
-//     }
-//
-//
-//
-// }
-//
-// window.addEventListener("keydown", keyDownHandler)
+const insert = document.querySelector("#insert");
+
+
+const eventHandler = (e) => {
+    insert.innerHTML=""
+
+    const keyCodes = {
+        "e.key": e.key === " " ? "Spaсe" : e.key,
+        "e.keyCode": e.keyCode,
+        "e.code": e.code,
+    }
+
+    console.log(keyCodes) //{e.key: 'f', e.keyCode: 70, e.code: 'KeyF'}
+
+    for (let key in keyCodes) {
+        const div = document.createElement('div');
+        div.className = "key"
+        div.textContent = keyCodes[key]
+
+
+        const small = document.createElement('small')
+        small.textContent =key
+
+
+        div.appendChild(small);
+        insert.appendChild(div)
+
+    }
+
+
+}
+
+
+window.addEventListener("keydown", eventHandler)
