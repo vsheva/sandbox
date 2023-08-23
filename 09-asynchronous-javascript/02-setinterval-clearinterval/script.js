1c330280ceea6132832900d6a9b3639e9413b94c
vsheva <valera.sheva@gmail.com>
2023-08-22 21:47:46 -0400

    Solution

---
+++ 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
@@ -0,0 +1,34 @@
+let intervalID;
+
+
+const changeColors = () => {
+    if(!intervalID ) {
+        intervalID = setInterval(changeRandomColors, 1000);
+    }
+}
+
+// const makeColor = () => {
+//     if ( document.body.style.backgroundColor !=="black") {
+//         document.body.style.backgroundColor ="black";
+//         document.body.style.color ="white";
+//     } else{
+//         document.body.style.backgroundColor ="white";
+//         document.body.style.color ="black";
+//     }
+// }
+
+
+const changeRandomColors =()=> {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+        document.body.style.backgroundColor = `#${randomColor}`
+
+}
+
+
+
+
+
+const stop = () =>clearInterval(intervalID)
+
+    document.querySelector("#start").addEventListener("click", changeColors)
+    document.querySelector("#stop").addEventListener("click", stop)
