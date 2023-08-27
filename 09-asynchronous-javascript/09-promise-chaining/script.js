1463548da1d00680ea14fce72ef1510dfa33d640
vsheva <valera.sheva@gmail.com>
2023-08-27 19:49:01 -0400

    Solution

--- 09-asynchronous-javascript/09-promise-chaining/script.js
+++ 09-asynchronous-javascript/09-promise-chaining/script.js
@@ -1,17 +1,28 @@
 const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    let error = false;
+    setTimeout(() => {
+        let error = true;
 
-    if (!error) {
-      resolve({ name: 'John', age: 30 });
-    } else {
-      reject('Error: Something went wrong');
-    }
-  }, 1000);
+        if (!error) {
+            resolve({name: 'John', age: 30});
+        } else {
+            reject('Error: Something went wrong');
+        }
+    }, 1000);
 });
 
 promise
-  .then((user) => {
-    console.log(user);
-  })
-  .catch((error) => console.log(error))
+    .then((user) => {
+        console.log(user);
+        return user.name
+    })
+    .then((name) => {
+        console.log(name);
+        return name.length
+    }).then((nLength) => {
+    console.log(nLength)
+})
+    .catch((error) => {
+        console.log(error);
+        return 123123
+    })
+    .then((num)=> {console.log("This will run no matter what", num)})
