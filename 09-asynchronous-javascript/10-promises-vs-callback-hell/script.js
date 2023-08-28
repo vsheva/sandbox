2174887269e37ea21b6ef92b4d60401cf5972deb
vsheva <valera.sheva@gmail.com>
2023-08-27 21:50:14 -0400

    Solution

--- 09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
+++ 09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
@@ -1,25 +1,47 @@
 function getData(endpoint, cb) {
-  const xhr = new XMLHttpRequest();
+    return new Promise(function (resolve, reject) {
+        const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', endpoint);
+        xhr.open('GET', endpoint);
 
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      cb(JSON.parse(this.responseText));
-    }
-  };
+        xhr.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    resolve(JSON.parse(this.responseText));
+                } else {
+                    reject("Something went wrong")
+                }
+            }
+        };
 
-  setTimeout(() => {
-    xhr.send();
-  }, Math.floor(Math.random() * 3000) + 1000);
+        setTimeout(() => {
+            xhr.send();
+        }, Math.floor(Math.random() * 3000) + 1000);
+    })
 }
 
-getData('./movies.json', (data) => {
-  console.log(data);
-  getData('./actors.json', (data) => {
-    console.log(data);
-    getData('./directors.json', (data) => {
-      console.log(data);
-    });
-  });
-});
+getData('./movies.json')
+    .then((movies) => {
+    console.log(movies);
+    return getData('./actors.json')
+})
+    .then((actors)=>{
+    console.log(actors);
+    return getData('./directors.json') //!!! ./directors1.json
+})
+    .then((directors)=>{
+    console.log(directors);
+})
+    .catch((err) =>console.log(err))
+
+
+
+// getData('./movies.json', (data) => {
+//   console.log(data);
+//   getData('./actors.json', (data) => {
+//     console.log(data);
+//     getData('./directors.json', (data) => {
+//       console.log(data);
+//     });
+//   });
+// });
