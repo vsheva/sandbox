a14387a408129ea60904ee8f65bdae3d0179a4cd
vsheva <valera.sheva@gmail.com>
2023-09-04 18:53:46 -0400

    Solution

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,51 +1,53 @@
 const apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
-const getTodos = () => {
-    fetch(apiUrl + "?_limit=5")
-        .then((response) => response.json())
+const getData = () => {
+    fetch(apiUrl + "?_limit=7")
+        .then((res) => res.json())
         .then((data) => {
-           data.map((el)=>addTodoToUI(el))  //!!!
+            //console.log(data);
+            data.map((todo) => {
+                showTodo(todo)
+            })
         })
 }
 
 
-const addTodoToUI = (todo) => {
-        const div = document.createElement("div");
-        div.textContent = todo.title;
-        div.setAttribute("data-id", todo.id)
+const showTodo = (todo) => {
+    const div = document.createElement("div");
+    div.textContent = todo.title
+    div.setAttribute("data-id", todo.id);
 
-        if (todo.completed) {
-            div.classList.add("done")
-        }
-        document.getElementById("todo-list").appendChild(div);
+    if (todo.completed) {
+        div.classList.add("done")
+    }
 
+    document.getElementById("todo-list").appendChild(div);
 }
 
 const createTodo = (e) => {
-    e.preventDefault();//console.log(e.target.firstElementChild.value)
+    e.preventDefault();
+
+    const newTodo = {
+        title: document.getElementById("inp").value, //!
+        completed: false,
+    }
 
     fetch(apiUrl, {
         method: "POST",
-
-        body: JSON.stringify({
-            title: document.getElementById("inp").value,
-            completed: false
-        }),
-
+        body: JSON.stringify(newTodo),
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
     })
         .then((res) => res.json())
-        .then((data) => {
-            addTodoToUI(data)
+        .then((newTodo) => {
+            showTodo(newTodo)
         })
-
 }
 
-const initTodo = () => {
-    document.addEventListener("DOMContentLoaded", getTodos)
-    document.getElementById("todo-form").addEventListener("submit", createTodo)
+const init = () => {
+    document.addEventListener("DOMContentLoaded", getData);
+    document.getElementById("todo-form").addEventListener("submit", createTodo);
 }
 
-initTodo()
+init();
