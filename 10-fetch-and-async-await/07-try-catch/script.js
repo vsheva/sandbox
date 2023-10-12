ed6565e38fda7234f50ff29350a5199505ec2b3e
vsheva <valera.sheva@gmail.com>
2023-10-11 23:07:39 -0400

    Solution

--- 10-fetch-and-async-await/07-try-catch/script.js
+++ 10-fetch-and-async-await/07-try-catch/script.js
@@ -1,23 +1,23 @@
-// try {
-//     console.log(x)
-// } catch (err) {
-//     console.log("Error: " + err)
-// }
-
-//console.log(b)
-
-
-function double(number) {
-    if (isNaN(number)) {
-        throw new Error(number + " is not a number")
-    }
-    return number * 2
-}
-
-
 try {
-    let a = double("test")
-    console.log(a)
-} catch(err) {
-    console.log(err)
+    console.log(x)
+} catch (err) {
+    console.log("Error: " + err)
 }
+
+console.log(b)
+
+//
+// function double(number) {
+//     if (isNaN(number)) {
+//         throw new Error(number + " is not a number")
+//     }
+//     return number * 2
+// }
+//
+//
+// try {
+//     let a = double("test")
+//     console.log(a)
+// } catch(err) {
+//     console.log(err)
+// }
